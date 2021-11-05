e7dfdd412cd45e0e16e6cec0671cc7d4feabad6c
venilson1 <venilson2009@hotmail.com>
2021-11-04 21:26:28 -0300

    security config

---
+++ backend/src/main/java/com/devSuperior/appVendas/AppVendasApplication.java
@@ -0,0 +1,13 @@
+package com.devSuperior.appVendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppVendasApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devSuperior/appVendas/AppVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devSuperior.appVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
