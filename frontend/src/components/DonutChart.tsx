e97006a3f1f50ba748736e64710c94c50848328a
venilson1 <venilson2009@hotmail.com>
2021-11-09 19:46:34 -0300

    DonutChart Integration

--- frontend/src/components/DonutChart.tsx
+++ frontend/src/components/DonutChart.tsx
@@ -29,11 +29,6 @@ export default function DonutChart() {
     });
   }, []);
 
-  const mockData = {
-    series: [477138, 499928, 444867, 220426, 473088],
-    labels: ["Anakin", "Barry Allen", "Kal-El", "Logan", "Padmé"],
-  };
-
   const options = {
     legend: {
       show: true,
@@ -42,8 +37,8 @@ export default function DonutChart() {
 
   return (
     <Chart
-      options={{ ...options, labels: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, labels: chartData.labels }}
+      series={chartData.series}
       type="donut"
       height={240}
     />
